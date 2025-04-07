8dd668b71974caa5ba9d50bb3cf65a1c322b5c8c
Linguardium <61733861+Linguardium@users.noreply.github.com>
2025-04-07 07:35:26 -0500

    cherry pick code style changes

--- src/main/java/mod/linguardium/trpal/mixin/QuantumSuitArmorMixin.java
+++ src/main/java/mod/linguardium/trpal/mixin/QuantumSuitArmorMixin.java
@@ -2,27 +2,21 @@ package mod.linguardium.trpal.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Equipment;
 import net.minecraft.item.ItemStack;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import reborncore.common.powerSystem.RcEnergyTier;
+import reborncore.common.powerSystem.RcEnergyItem;
 import techreborn.items.armor.QuantumSuitItem;
-import techreborn.items.armor.TREnergyArmourItem;
 
 import static mod.linguardium.trpal.QuantumChestplatePAL.flightTick;
 import static mod.linguardium.trpal.QuantumChestplatePAL.onUnequip;
 
 @Mixin(QuantumSuitItem.class)
-public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
-
-	public QuantumSuitArmorMixin(ArmorMaterial material, Type slot, long maxCharge, RcEnergyTier energyTier) {
-		super(material, slot, maxCharge, energyTier);
-	}
+public abstract class QuantumSuitArmorMixin implements Equipment {
 
 	@ModifyExpressionValue(
 			method="tickArmor",
@@ -31,15 +25,14 @@ public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
 					target = "Ltechreborn/config/TechRebornConfig;quantumSuitEnableFlight:Z",
 					remap = false)
 	)
-	private boolean redirectFlightLogic(boolean flightConfigurationEnabled, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) PlayerEntity player) {
-		if (player == null || stack == null) {
-			return flightConfigurationEnabled;
-		}
-		flightTick(flightConfigurationEnabled, player, stack,this);
+    private boolean redirectFlightLogic(boolean flightConfigurationEnabled, @Local(argsOnly = true) ItemStack stack, @Local(argsOnly = true) PlayerEntity player) {
+        if (!(stack.getItem() instanceof RcEnergyItem energyItem)) {
+            return flightConfigurationEnabled;
+        }
+        flightTick(flightConfigurationEnabled, player, stack, energyItem);
 		// disable original code
 		return false;
 	}
-
 
 	@ModifyExpressionValue(
 			method="onRemoved",
@@ -51,8 +44,8 @@ public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
 	private boolean removeFlightAbilityOnUnequip(boolean quantumSuitEnableFlight, @Local(argsOnly = true) PlayerEntity player) {
 		if (this.getSlotType() == EquipmentSlot.CHEST) {
 			onUnequip(player);
-			// disable original code
 		}
+		// disable original code
 		return false;
 	}
 }
