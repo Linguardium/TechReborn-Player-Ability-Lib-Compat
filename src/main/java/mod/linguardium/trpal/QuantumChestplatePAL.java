341f4dcfb29abda338594325acba535c8a9c6c56
Linguardium <61733861+Linguardium@users.noreply.github.com>
2025-04-07 07:35:26 -0500

    minor code style changes use ServerPlayerEntity explicitly instead of isClient checks bugfix: PAL provided instant build mode provides free flight update to 1.21.1

--- src/main/java/mod/linguardium/trpal/QuantumChestplatePAL.java
+++ src/main/java/mod/linguardium/trpal/QuantumChestplatePAL.java
@@ -5,66 +5,72 @@ import io.github.ladysnake.pal.Pal;
 import io.github.ladysnake.pal.VanillaAbilities;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
+import reborncore.common.powerSystem.RcEnergyItem;
 import techreborn.config.TechRebornConfig;
-import techreborn.items.armor.TREnergyArmourItem;
 
 public class QuantumChestplatePAL {
 
-    private static final Identifier QUANTUM_ARMOR_FLIGHT_ABILITY_SOURCE_ID = new Identifier("techreborn", "quantum_armor");
+    private static final Identifier QUANTUM_ARMOR_FLIGHT_ABILITY_SOURCE_ID = Identifier.of("techreborn", "quantum_armor");
     private static final AbilitySource TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE = Pal.getAbilitySource(QUANTUM_ARMOR_FLIGHT_ABILITY_SOURCE_ID, AbilitySource.CONSUMABLE);
 
-    public static void flightTick(boolean flightConfigEnabled, PlayerEntity player, ItemStack stack, TREnergyArmourItem item) {
+    public static void flightTick(boolean flightConfigEnabled, PlayerEntity player, ItemStack stack, RcEnergyItem item) {
         // PAL abilities are only valid on serverside
-        if (!player.getWorld().isClient()) {
-            if (flightConfigEnabled) {
-                // This check does not allow for exact energy match. This is copied as-is to match TechReborn's implementation
-                // This mod is not intended to fix bugs in TR
-                if (item.getStoredEnergy(stack) > TechRebornConfig.quantumSuitFlyingCost) {
-                    allowFlying(player);
-                } else {
-                    dontAllowFlying(player);
-                }
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) return;
 
-                if (!hasIndirectFlight(player) && VanillaAbilities.FLYING.isEnabledFor(player) && isAllowingFlight(player)) {
-                    item.tryUseEnergy(stack, TechRebornConfig.quantumSuitFlyingCost);
-                }
-
-            }else{
-                // in case someone disables flight in config while granted flight
-                dontAllowFlying(player);
-            }
+        if (!flightConfigEnabled) {
+            dontAllowFlying(serverPlayer);
+            return;
         }
+
+        // This check does not allow for exact energy match. This is copied as-is to match TechReborn's implementation
+        // This mod is not intended to fix bugs in TR
+        if (item.getStoredEnergy(stack) > TechRebornConfig.quantumSuitFlyingCost) {
+            allowFlying(serverPlayer);
+        } else {
+            dontAllowFlying(serverPlayer);
+        }
+
+        if (!isCurrentlyFlying(serverPlayer)) return;
+        if (!isAllowingFlight(serverPlayer)) return;
+
+        if (hasIndirectFlight(serverPlayer)) return;
+
+        item.tryUseEnergy(stack, TechRebornConfig.quantumSuitFlyingCost);
+
     }
 
     // remove flight allowance when unequipped
     public static void onUnequip(PlayerEntity player) {
-        dontAllowFlying(player);
+        if (player instanceof ServerPlayerEntity serverPlayer) {
+            dontAllowFlying(serverPlayer);
+        }
     }
 
     // checks if the Quantum Chestplate's Ability Source is allowing flight
     // checks if the Quantum Chestplate is the highest priority flight allowance granter
-    public static boolean isAllowingFlight(PlayerEntity player) {
+    public static boolean isAllowingFlight(ServerPlayerEntity player) {
         return TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.grants(player, VanillaAbilities.ALLOW_FLYING) && TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.isActivelyGranting(player, VanillaAbilities.ALLOW_FLYING);
     }
 
-    // checks if the player has create mode or is a spectator to attempt to verify if the player should fly for free
+    // checks if the player has creative mode or is a spectator to attempt to verify if the player should fly for free
     public static boolean hasIndirectFlight(PlayerEntity player) {
-        return VanillaAbilities.CREATIVE_MODE.isEnabledFor(player) || player.isCreative() || player.isSpectator();
+        return player.isCreative() || player.isSpectator();
     }
 
     // Enable Quantum Chestplate Ability Source to allow flight
-    public static void allowFlying(PlayerEntity playerEntity) {
-        if (!playerEntity.getWorld().isClient()) {
-            TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.grantTo(playerEntity, VanillaAbilities.ALLOW_FLYING);
-            playerEntity.setOnGround(true);
-        }
+    public static void allowFlying(ServerPlayerEntity playerEntity) {
+        TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.grantTo(playerEntity, VanillaAbilities.ALLOW_FLYING);
+        playerEntity.setOnGround(true);
     }
 
     // Disable Quantum Chestplate Ability Source from allowing flight
-    public static void dontAllowFlying(PlayerEntity playerEntity) {
-        if (!playerEntity.getWorld().isClient()) {
-            TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.revokeFrom(playerEntity, VanillaAbilities.ALLOW_FLYING);
-        }
+    public static void dontAllowFlying(ServerPlayerEntity playerEntity) {
+        TECHREBORN_QUANTUM_ARMOR_ABILITY_SOURCE.revokeFrom(playerEntity, VanillaAbilities.ALLOW_FLYING);
+    }
+
+    public static boolean isCurrentlyFlying(ServerPlayerEntity player) {
+        return VanillaAbilities.FLYING.isEnabledFor(player);
     }
 }
--- src/main/java/mod/linguardium/trpal/mixin/QuantumSuitArmorMixin.java
+++ src/main/java/mod/linguardium/trpal/mixin/QuantumSuitArmorMixin.java
@@ -3,24 +3,19 @@ package mod.linguardium.trpal.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Equipment;
 import net.minecraft.item.ItemStack;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import reborncore.common.powerSystem.RcEnergyTier;
+import reborncore.common.powerSystem.RcEnergyItem;
 import techreborn.items.armor.QuantumSuitItem;
-import techreborn.items.armor.TREnergyArmourItem;
 
 import static mod.linguardium.trpal.QuantumChestplatePAL.flightTick;
 import static mod.linguardium.trpal.QuantumChestplatePAL.onUnequip;
 
 @Mixin(QuantumSuitItem.class)
-public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
-
-	public QuantumSuitArmorMixin(ArmorMaterial material, Type slot, long maxCharge, RcEnergyTier energyTier) {
-		super(material, slot, maxCharge, energyTier);
-	}
+public abstract class QuantumSuitArmorMixin implements Equipment {
 
 	@ModifyExpressionValue(
 			method="tickArmor",
@@ -30,11 +25,12 @@ public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
 					remap = false)
 	)
 	private boolean redirectFlightLogic(boolean flightConfigurationEnabled, ItemStack stack, PlayerEntity player) {
-		flightTick(flightConfigurationEnabled, player,stack,this);
+		if (stack.getItem() instanceof RcEnergyItem energyItem) {
+			flightTick(flightConfigurationEnabled, player, stack, energyItem);
+		}
 		// disable original code
 		return false;
 	}
-
 
 	@ModifyExpressionValue(
 			method="onRemoved",
@@ -46,8 +42,8 @@ public abstract class QuantumSuitArmorMixin extends TREnergyArmourItem {
 	private boolean removeFlightAbilityOnUnequip(boolean quantumSuitEnableFlight, PlayerEntity player) {
 		if (this.getSlotType() == EquipmentSlot.CHEST) {
 			onUnequip(player);
-			// disable original code
 		}
+		// disable original code
 		return false;
 	}
 }
